e57fceb1335cff3cdd1687748b4a2ab1d349fe00
Rushik005 <90761109+Rushik005@users.noreply.github.com>
2025-10-02 11:53:29 +0530

    Update postcheck.sh

--- playbooks/pre-post checks/postcheck.sh
+++ playbooks/pre-post checks/postcheck.sh
@@ -45,19 +45,5 @@ echo "-----------------------------">> /tmp/post_check_$DATE
 sestatus>> /tmp/post_check_$DATE
 echo "-----------------------------">> /tmp/post_check_$DATE
 
-ps -ef | grep [p]mon 1>/dev/null 
-db=$?
-if [ $db == 0 ];then
-echo "DB running on the server"
-else
-echo "No DB running "
-fi
-ps -ef | grep [m]ysql 1>/dev/null
-my=$?
-if [ $my == 0 ];then
-echo "MYSQL running on the server"
-else
-echo "No MYSQL running "
-fi
 
-diff -y /tmp/pre_check_* /tmp/post_check_$DATE -W 150 | sed -e "s/.*|.*/\x1b[44m&\x1b[0m/" -e "s/.*>.*/\x1b[44m&\x1b[0m/"
+diff -y /tmp/pre_check_* /tmp/post_check_$DATE -W 150 | sed -e "s/.*|.*/\x1b[44m&\x1b[0m/" -e "s/.*>.*/\x1b[44m&\x1b[0m/" >> /tmp/difference_$DATE
